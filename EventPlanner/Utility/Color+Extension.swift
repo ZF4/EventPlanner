ac5c8d012d7e96f054b50d601e48d8a1891c67f4
Alandis <1seals.alandis@gmail.com>
2023-07-11 11:00:45 -0500

    Added app Colors To the project

---
+++ EventPlanner/Utility/Color+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Extension.swift
+//  EventPlanner
+//
+//  Created by Alandis Seals on 7/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static var amour = Color("amour")
+    static var baseBlue = Color("baseBlue")
+    static var baseRed = Color("baseRed")
+    static var burgany = Color("burgany")
+    static var darkBllue = Color("darkBllue")
+}
